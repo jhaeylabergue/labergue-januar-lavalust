1b84de773e1e38bc1664d281b3bf0e0ca1ff4e04
Januar <jhaeylabergue@gmail.com>
2026-09-05 03:05:27 -0400

    Fix Aiven MySQL deployment config

--- app/config/database.php
+++ app/config/database.php
@@ -58,18 +58,18 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 $database['main'] = array(
-    'driver'	=> getenv('DB_DRIVER') ?: 'mysql',
-    'hostname'	=> getenv('DB_HOST') ?: '',
-    'port'		=> getenv('DB_PORT') ?: '',
-    'username'	=> getenv('DB_USERNAME') ?: (getenv('DB_USER') ?: ''),
-    'password'	=> getenv('DB_PASSWORD') ?: '',
-    'database'	=> getenv('DB_DATABASE') ?: (getenv('DB_NAME') ?: ''),
-    'charset'	=> getenv('DB_CHARSET') ?: 'utf8mb4',
-    'dbprefix'	=> '',
-    'ssl_ca'	=> getenv('DB_SSL_CA') ?: '',
-    'ssl_verify'=> getenv('DB_SSL_VERIFY') === false ? true : getenv('DB_SSL_VERIFY'),
+    'driver'    => getenv('DB_DRIVER') ?: 'mysql',
+    'hostname'  => getenv('DB_HOST') ?: '',
+    'port'      => getenv('DB_PORT') ?: '',
+    'username'  => getenv('DB_USERNAME') ?: '',
+    'password'  => getenv('DB_PASSWORD') ?: '',
+    'database'  => getenv('DB_DATABASE') ?: 'defaultdb',
+    'charset'   => getenv('DB_CHARSET') ?: 'utf8mb4',
+    'dbprefix'  => '',
+    'ssl_ca'    => getenv('DB_SSL_CA') ?: '',
+    'ssl_verify'=> getenv('DB_SSL_VERIFY') !== false ? getenv('DB_SSL_VERIFY') : true,
     // Optional for SQLite
-    'path'      => ''
+    'path'      => getenv('DB_PATH') ?: ''
 );
 
 ?>
